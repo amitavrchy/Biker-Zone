3513528a1382464a82970a50997610d769b0197e
amitavrchy <amitavrchy01@gmail.com>
2024-01-21 23:23:55 +0600

    hero part

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'bg-bike-primary': '#264653',
+        }
+      },
+    },
+    plugins: [],
+  }
